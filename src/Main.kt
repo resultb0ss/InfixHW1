06ae02a90e58980ccc4e430ada5a726562c35f46
Admin <reteamboss@gmail.com>
2024-09-23 15:53:02 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
 fun main() {
-    println("Hello World!")
+
+    var numOne = 10
+    var numTwo = 23
+
+    println(numOne mySum numTwo)
+    println(numOne myDev numTwo)
+    println(numOne myDef numTwo)
+    println(numOne myMul numTwo)
+}
+
+infix fun Int.mySum(num: Int):Int{
+    var result = this + num
+    return result
+}
+
+infix fun Int.myDef(num: Int):Int{
+    var result = this - num
+    return result
+}
+
+infix fun Int.myDev(num: Int):Double{
+    var result = this.toDouble() / num.toDouble()
+    return result
+}
+
+infix fun Int.myMul(num: Int):Int{
+    var result = this * num
+    return result
 }
